98d43173e81adf2102294ef48b73702aa1ed07d7
Rui Gaspar <ruigaspar03@hotmail.com>
2022-11-03 15:23:18 +0100

    Form to create a job

--- skeleton/src/Controller/JobsController.php
+++ skeleton/src/Controller/JobsController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Jobs;
+use App\Entity\User;
 use App\Repository\JobsRepository;
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Query;
 
 class JobsController extends AbstractController
 {
@@ -21,32 +25,54 @@ class JobsController extends AbstractController
 
         $job = new Jobs();
         $job->setJobTitle('React Native|JS Developer');
-        $job->setJobDescription('React Native or JS Developer for a web application to a mobile application with a 6 months contract'); 
+        $job->setJobDescription('React Native or JS Developer for a web application to a mobile application with a 6 months contract');
         $job->setJobCreator('Mark Zuckerberg');
         $job->setJobLocation('London');
         $job->setJobDegree('Master 1');
-        $job->setJobPostDate(new \DateTime());        
-        $job->setJobName('React Native|JS Developer');
-        
+        $job->setJobPostDate(new \DateTime());
+
 
         $entityManager->persist($job);
         $entityManager->flush();
 
         return new Response('Saved new job with id ' . $job->getId());
-    
+
     }
 
     #[Route('/jobs', name: 'jobs_form')]
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+
+        $skills = [
+            'PHP' => 'PHP',
+            'Java' => 'Java',
+            'C#' => 'C#',
+            'C++' => 'C++',
+            'Python' => 'Python',
+            'Ruby' => 'Ruby',
+            'JavaScript' => 'JavaScript',
+            'C' => 'C',
+            'Go' => 'Go',
+            'Rust' => 'Rust',
+            'Swift' => 'Swift',
+            'Kotlin' => 'Kotlin',
+            'Dart' => 'Dart',
+            'Lua' => 'Lua',
+            'Assembly' => 'Assembly',
+            'Scratch' => 'Scratch',
+        ];
+
         $form = $this->createFormBuilder()
-            ->add('jobName', TextType::class)
             ->add('jobCreator', TextType::class)
-//            ->add('jobPostDate', DateType::class)
             ->add('jobDegree', TextType::class)
             ->add('jobDescription', TextType::class)
             ->add('jobTitle', TextType::class)
             ->add('jobLocation', TextType::class)
+            ->add('Skills', ChoiceType::class, [
+                'choices' => $skills,
+                'multiple' => true,
+                'expanded' => true,
+            ])
             ->getForm();
 
         $form->handleRequest($request);
@@ -55,18 +81,19 @@ class JobsController extends AbstractController
             $data = $form->getData();
             $entityManager = $doctrine->getManager();
             $job = new Jobs();
-            $job->setJobName($data['jobName']);
             $job->setJobCreator($data['jobCreator']);
             $job->setJobPostDate(new \DateTime());
             $job->setJobDegree($data['jobDegree']);
             $job->setJobDescription($data['jobDescription']);
             $job->setJobTitle($data['jobTitle']);
             $job->setJobLocation($data['jobLocation']);
+            $job->setJobSkills($data['Skills']);
             $entityManager->persist($job);
             $entityManager->flush();
 
             return $this->redirectToRoute('jobs_render');
 
+//            dd($data);
         }
 
         return $this->render('jobs/index.html.twig', [
@@ -76,17 +103,24 @@ class JobsController extends AbstractController
 
 
     }
-    
+
 
     #[Route('/jobsRender', name: 'jobs_render')]
-    public function readJob(ManagerRegistry $doctrine): Response
+    public function readJob(Request $request, ManagerRegistry $doctrine): Response
     {
         $jobRepository = $doctrine->getRepository(Jobs::class)->findAll();
 
+//        for ($i = 0; $i < count($jobRepository); $i++) {
+//            $arr = $jobRepository[$i]->getJobSkills();
+//        }
+//
+//        dd($arr);
 
         return $this->render('jobs/show.html.twig', [
             'jobs' => $jobRepository,
+//            'users' => $userRepository,
         ]);
+
     }
 
 
@@ -110,14 +144,4 @@ class JobsController extends AbstractController
     }
 
 
-
-//    #[Route('/jobsOrderByDes', name: 'jobs_order_by_desc')]
-//    public function jobByDesc(ManagerRegistry $doctrine): Response
-//    {
-//        $jobRepository = $doctrine->getRepository(Jobs::class)->orderBy();
-//        return $this->render('jobs/index.html.twig', [
-//            'jobs' => $jobRepository,
-//        ]);
-//    }
-    
 }
--- skeleton/src/Repository/JobsRepository.php
+++ skeleton/src/Repository/JobsRepository.php
@@ -44,9 +44,35 @@ class JobsRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('j')
             ->orderBy('j.jobDegree', 'DESC')
             ->getQuery()
-            ->getResult()
-        ;
-        
+            ->getResult();
+
+    }
+
+    public function orderByMatch($jobSkill, $userSkill)
+    {
+        return $this->createQueryBuilder('j')
+            ->setParameters(['jobSkills' => $jobSkill, 'userSkills' => $userSkill])
+            ->where('j.jobSkills = :jobSkills')
+            ->andWhere('j.jobSkills = :userSkills')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function orderBySkill($jobSkill)
+    {
+        return $this->createQueryBuilder('j')
+            ->setParameters(['jobSkills' => $jobSkill])
+            ->where('j.jobSkills = :jobSkills')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getJobsSkills()
+    {
+        return $this->createQueryBuilder('j')
+            ->select('j.jobSkills')
+            ->getQuery()
+            ->getResult();
     }
 
 //    /**
