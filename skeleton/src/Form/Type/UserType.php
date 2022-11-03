e5521327005be362bbb5c260d16b616743b500ac
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-11-03 14:09:41 +0100

    header + form

--- skeleton/src/Form/Type/UserType.php
+++ skeleton/src/Form/Type/UserType.php
@@ -4,7 +4,9 @@ namespace App\Form\Type;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,12 +18,12 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options) : void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('age')
-            ->add('address')
-            ->add('levels')
-            ->add('save', SubmitType::class, ['label' => 'Create User'])
+            ->add('name', TextType::class, ['label' => 'Name'])
+            ->add('surname', TextType::class, ['label' => 'Surname'])
+            ->add('age', IntegerType::class, ['label' => 'Age'])
+            ->add('address', TextType::class, ['label' => 'Address'])
+            ->add('levels', IntegerType::class, ['label' => 'Levels'])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer et continuer'])
         ;
     }
 
