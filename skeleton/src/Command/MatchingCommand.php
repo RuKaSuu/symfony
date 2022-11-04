65397bd80900e5230099148904dc02ce89bb41ab
Rui Gaspar <ruigaspar03@hotmail.com>
2022-11-04 16:47:47 +0100

    Adding matching command

--- skeleton/src/Command/MatchingCommand.php
+++ skeleton/src/Command/MatchingCommand.php
@@ -1,67 +1,79 @@
 <?php
 
+namespace App\Command;
+
 use App\Entity\Jobs;
 use App\Entity\User;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
-// create a cli command for checking all jobs who dosen't match with the user's skills
 #[AsCommand(
     name: 'app:matching',
-    description: 'Checks all jobs who dosen\'t match with the user\'s skills',
+    description: 'Checks if a user exists in the database or not',
 )]
-
 class MatchingCommand extends Command
 {
-    private $entityManager;
-    private $io;
+    private EntityManagerInterface $entityManager;
 
-        public function __construct(EntityManagerInterface $entityManager)
-        {
-            parent::__construct();
-            $this->entityManager = $entityManager;
-        }
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
 
-        protected function configure(): void
-        {
-            $this
-                ->setHelp($this->getCommandHelp());
-            // commands can optionally define arguments and/or options (mandatory and optional)
-            // see https://symfony.com/doc/current/components/console/console_arguments.html
+    protected function configure(): void
+    {
+        $this
+            ->setHelp($this->getCommandHelp());
+        // commands can optionally define arguments and/or options (mandatory and optional)
+        // see https://symfony.com/doc/current/components/console/console_arguments.html
+    }
 
-        }
+    private function getCommandHelp(): string
+    {
+        return <<<EOF
+        The <info>%command.name%</info> command checks if a user exists in the database or not.
+        EOF;
 
-        protected function execute(InputInterface $input, OutputInterface $output): int
-        {
-            $em = $this->entityManager;
+    }
 
-            $helper = $this->getHelper('question');
-            $question = new Question('Please enter the name of the company: ');
-            $name = $helper->ask($input, $output, $question);
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $em = $this->entityManager;
 
-            // check all jobs who dosen't match with all user's skills
-            $jobs = $em->getRepository(Jobs::class)->findBy(['company' => $name]);
-            $users = $em->getRepository(User::class)->findAll();
+        $helper = $this->getHelper('question');
+        $question = new Question('Please enter the id of the user: ');
+        $name = $helper->ask($input, $output, $question);
+        $output->writeln('---------------------------------------------');
 
+        // check all jobs who don't match with all user's skills
+        $jobs = $em->getRepository(Jobs::class)->findBy(['company' => $name]);
+        $users = $em->getRepository(User::class)->findAll();
+
+
+        foreach ($users as $user) {
             foreach ($jobs as $job) {
-                foreach ($users as $user) {
-                    if ($job->getSkills() != $user->getSkills()) {
-                        $output->writeln('Job: '.$job->getTitle());
-                        $output->writeln('Company: '.$job->getCompany());
-                        $output->writeln('Skills: '.$job->getSkills());
-                        $output->writeln('Location: '.$job->getLocation());
-                        $output->writeln('Description: '.$job->getDescription());
-                        $output->writeln('Salary: '.$job->getSalary());
-                        $output->writeln(' ');
-                    }
+                if (!array_intersect($job->getSkills(), $user->getSkills())) {
+                    $date = $job->getPostDate();
+                    $output->writeln('Title: ' . $job->getTitle());
+                    $output->writeln('PostDate: ' . $date->format('d-m-Y'));
+                    $output->writeln('Degree: ' . $job->getDegree());
+                    $output->writeln('Description: ' . $job->getDescription());
+                    $output->writeln('Location: ' . $job->getLocation());
+                    $output->writeln('Skills: ' . implode(', ', $job->getSkills()));
+                    $output->writeln('Company: ' . $job->getCompany());
+                    $output->writeln('------------------------------------------------------------------');
+                }else{
+                    $output->writeln('No jobs found');
                 }
-            }
 
-            return Command::SUCCESS;
+            }
         }
 
-
+        return Command::SUCCESS;
+    }
 }
